1974700d898fc1e08ed5a60ee7a296d196817fdb
EzioAuditore12 <dakshpurohit2005@gmail.com>
2026-02-23 00:12:38 +0530

    -a

--- mobile/src/app/(main)/new-chat/[id].tsx
+++ mobile/src/app/(main)/new-chat/[id].tsx
@@ -1,4 +1,5 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
+import { Avatar } from 'heroui-native/avatar';
 import { Description } from 'heroui-native/description';
 import { View } from 'react-native';
 
@@ -12,7 +13,15 @@ export default function NewChattingScreen() {
     <>
       <Stack.Screen
         options={{
-          header: () => <View className="flex-row gap-x-2 p-2"></View>,
+          header: () => (
+            <View className="flex-row gap-x-2 p-2">
+              <Avatar alt={name}>
+                <Avatar.Image />
+                <Avatar.Fallback>{name[0]}</Avatar.Fallback>
+              </Avatar>
+              <Description>{name}</Description>
+            </View>
+          ),
         }}
       />
       <View className="flex-1 items-center justify-center">
